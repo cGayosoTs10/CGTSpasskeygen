ae5942d017c5e7157129a0fa5c2220cbae7f13fc
Cauê Gayoso Tavares da Silva <caue.gts10@tutamail.com>
2026-09-12 17:55:14 -0300

    Change: Adicionado assinatura no codigo fonte, como solicitado, pois havia esquecido.

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -4,6 +4,8 @@ import business.GeradorPin;
 import business.GeradorSenhaForte;
 import business.IGeradorSenha;
 
+// Feito por Cauê Gayoso Tavares da Silva, sem Dupla.
+
 public class Principal {
 
     public static void main(String[] args) {
@@ -16,7 +18,7 @@ public class Principal {
         System.out.println("Senha forte 2: " + gerador.gerarSenha());
 
         gerador = new GeradorPin();
-
+	System.out.println("Feito por Cauê Gayoso Tavares da Silva.");
         System.out.println("PIN 1: " + gerador.gerarSenha());
         System.out.println("PIN 2: " + gerador.gerarSenha()); // Debug basicão.
     }
